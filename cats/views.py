3a4ca56e13a6703f73b0399bb4ff88d5ae04accc
Daniil Klimov <x.kek97@mail.ru>
2025-09-10 11:40:50 +0300

    14 sprint 1 tema

--- cats/views.py
+++ cats/views.py
@@ -1,16 +1,28 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from rest_framework.throttling import ScopedRateThrottle
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Achievement, Cat, User
 
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
-
+from .permissions import OwnerOrReadOnly
+from .throttling import WorkingHoursRateThrottle
+from . pagination import CatsPagination
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
-
+    permission_classes = (OwnerOrReadOnly,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
+    throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
+    throttle_scope = 'low_request'
+    pagination_class = None
+    filterset_fields = ('color', 'birth_year')
+    search_fields = ('name',)
+    ordering_fields = ('name', 'birth_year')
+    ordering = ('birth_year',)
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+        serializer.save(owner=self.request.user)
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
